7753a02416d65da923190613edfb059f5eeb134d
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-06-23 16:57:34 -0700

    Check deserialization with extraneous fields

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,15 @@
-#[macro_use]
-extern crate map_struct;
+use serde::{Deserialize, Serialize};
+use serde_json::{from_value, json, to_string};
 
-use serde::Serialize;
-use serde_json::to_string;
-
-#[map(Option, #[serde(skip_serializing_if = "Option::is_none")])]
-#[derive(Debug, Serialize)]
-pub struct Foo {
-    #[serde(rename = "foofoo")]
-    foo: u32,
-    bar: u32
-}
-
-#[derive(Debug, Serialize)]
-pub struct Bar {
-    bar: u64
+#[derive(Deserialize, Serialize)]
+struct Foo {
+    a: u64
 }
 
 fn main() {
-    let foo: Foo = Foo {
-        foo: None,
-        bar: Some(20)
-    };
-
-    println!("{}", to_string(&foo).unwrap());
+    let f: Foo = from_value(json!({
+        "a": 10,
+        "b": 20
+    })).unwrap();
+    println!("Value: {}", to_string(&f).unwrap())
 }
