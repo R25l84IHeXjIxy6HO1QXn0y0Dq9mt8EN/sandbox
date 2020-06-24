23f3e77e9c296b1951f284fe27f28470bf499f3c
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-06-23 21:28:29 -0700

    `winapi` does not contain bindings for Unix sockets

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-use std::thread::{spawn, sleep};
-use std::time::Duration;
-use std::fs::File;
-
-use fs2::FileExt;
-
-fn worker() {
-    let f = File::open("foo.txt").unwrap();
-    f.lock_shared().unwrap();
-    println!("HELLO");
-    sleep(Duration::from_secs(5));
-}
+use winapi::um::winsock2;
 
 fn main() {
-    spawn(worker);
-    sleep(Duration::from_secs(1));
-    let f = File::open("foo.txt").unwrap();
-    f.lock_exclusive().unwrap();
+    let skt = winsock2::INVALID_SOCKET;
 }
