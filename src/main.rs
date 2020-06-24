07d02123d38facf986b9ccdaa9088724800098cd
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-06-23 18:50:02 -0700

    Remove Argon2 due to Windows build concerns

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-use serde::{Deserialize, Serialize};
-use serde_json::{from_value, json, to_string};
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-#[derive(Deserialize, Serialize)]
-struct Foo {
-    a: u64
-}
+use std::ffi::c_void;
+
+include!("../bindings.rs");
+
+const HASH_LEN: usize = 32;
+const SALT_LEN: usize = 0;
 
 fn main() {
-    let f: Foo = from_value(json!({
-        "a": 10,
-        "b": 20
-    })).unwrap();
-    println!("Value: {}", to_string(&f).unwrap())
+    let mut hash: [u8; HASH_LEN] = [0; HASH_LEN];
+    let mut salt: [u8; SALT_LEN] = [0; SALT_LEN];
+
+    let t_cost: u32 = 2;
+    let m_cost: u32 = 1 << 16;
+    let parallelism: u32 = 1;
+
+    let mut pwd = "password";
+
+    unsafe {
+        argon2i_hash_raw(
+            t_cost, m_cost, parallelism,
+            &mut pwd as *mut _ as *mut c_void, pwd.len() as u64,
+            &mut salt as *mut _ as *mut c_void, salt.len() as u64,
+            &mut hash as *mut _ as *mut c_void, hash.len() as u64);
+    }
 }
