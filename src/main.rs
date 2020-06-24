60de64f7a9d74f14400bf46279857d9e37d0cdd7
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-06-23 21:15:32 -0700

    File locking

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
-use std::ffi::CString;
-use std::os::raw::c_char;
+use std::thread::{spawn, sleep};
+use std::time::Duration;
+use std::fs::File;
 
-use libsodium_sys as sodium;
-use zeroize::Zeroizing;
+use fs2::FileExt;
+
+fn worker() {
+    let f = File::open("foo.txt").unwrap();
+    f.lock_shared().unwrap();
+    println!("HELLO");
+    sleep(Duration::from_secs(5));
+}
 
 fn main() {
-    unsafe {
-        sodium::sodium_init();
-    }
-
-    let pwd = Zeroizing::new(String::from("password"));
-    let mut buf = Zeroizing::new(vec![0 as c_char; sodium::crypto_pwhash_STRBYTES as usize]);
-
-    unsafe {
-        sodium::crypto_pwhash_str(
-            buf.as_mut_ptr(),
-            pwd.as_ptr() as *const c_char,
-            pwd.len() as u64,
-            sodium::crypto_pwhash_OPSLIMIT_INTERACTIVE as u64,
-            sodium::crypto_pwhash_MEMLIMIT_INTERACTIVE as usize
-        );
-        println!("{}", CString::from_raw(buf.as_mut_ptr()).into_string().unwrap());
-    }
+    spawn(worker);
+    sleep(Duration::from_secs(1));
+    let f = File::open("foo.txt").unwrap();
+    f.lock_exclusive().unwrap();
 }
