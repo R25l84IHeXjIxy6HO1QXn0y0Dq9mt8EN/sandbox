80d9dd9d98d656391df0cf0a522c6806c49fe704
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-06-24 13:05:53 -0700

    Temporary commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
-use winapi::um::winsock2;
+use std::fs;
+use std::io;
+
+use futures::{future, Future};
+use hyper::{Body, Request, Response, Server};
+use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
+use hyper::service::service_fn;
+
+async fn hello(_: Request<Body>) -> impl Future<Output = Result<Response<Body>, hyper::http::Error>> {
+    let foo = "FOO";
+    future::ready(
+        Response::builder()
+            .header(CONTENT_TYPE, "text/plain")
+            .header(CONTENT_LENGTH, foo.len())
+            .body(foo.into())
+    )
+}
+
+fn run() -> io::Result<()> {
+    let path = "foo.sock";
+    if let Err(err) = fs::remove_file(path) {
+        if err.kind() != io::ErrorKind::NotFound {
+            return Err(err);
+        }
+    }
+    let srv = Server::bind_unix(path, service_fn(hello))?;
+    srv.run()?;
+    Ok(())
+}
 
 fn main() {
-    let skt = winsock2::INVALID_SOCKET;
+    if let Err(err) = run() {
+        println!("Error starting server: {}", err)
+    }
 }
