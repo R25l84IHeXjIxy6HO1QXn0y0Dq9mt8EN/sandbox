21a8fed6dd5f3591bb784d5563ff657137ab2948
R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN <67037013+R25l84IHeXjIxy6HO1QXn0y0Dq9mt8EN@users.noreply.github.com>
2020-06-25 13:59:10 -0700

    Test Unix domain sockets

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::fs;
+use std::io;
 
 use futures::{future, Future};
 use hyper::{Body, Request, Response, Server};
@@ -8,6 +10,11 @@ use hyperlocal::UnixServerExt;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let path = "foo.sock";
+    if let Err(err) = fs::remove_file(path) {
+        if err.kind() != io::ErrorKind::NotFound {
+            panic!("FAILED TO REMOVE EXISTING SOCKET");
+        }
+    }
     let srv = Server::bind_unix(path)?
         .serve(make_service_fn(|_| async {
             Ok::<_, hyper::Error>(service_fn(|_req| async {
